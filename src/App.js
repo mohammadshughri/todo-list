2826e2e940e43cc45b48a612588b6491cb4ecea8
MohammadShughri <Mohammadws19@gmail.com>
2021-07-17 20:43:06 +0300

    Added context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
+import { ProjectsProvider, selectedProjectsProvider } from "./context";
 
-export const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
-  );
-};
+export const App = () => (
+  <selectedProjectsProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </selectedProjectsProvider>
+);
 
 export default App;
