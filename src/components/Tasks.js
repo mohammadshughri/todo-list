84d021b82f79a375a1a438fd9a186fff459ecce1
MohammadShughri <Mohammadws19@gmail.com>
2021-07-17 12:10:05 +0300

    Added Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Checkbox } from "./Checkbox";
 import { useTasks } from "../hooks";
+import { collatedTasks } from "../constants";
+import {
+  getTitle,
+  getCollatedTitle,
+  CollatedTasksExist,
+  collatedTasksExist,
+} from "../helpers";
+import { useSelectedProjectValue, useProjectValue } from "../context";
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
+  const { selectedProject } = useSelectedProjectValue();
+  const { tasks } = useTasks(selectedProject);
+  const { projects } = useProjectValue();
 
   console.log(tasks);
 
-
   let projectName = "";
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    console.log("projectName 1: ", projectName);
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    console.log("projectName 2: ", projectName);
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todist`;
+  }, []);
 
   return (
     <div className="tasks" data-testid="tasks">
